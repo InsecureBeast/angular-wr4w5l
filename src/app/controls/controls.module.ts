b3a10594309c8e84af36bff462927480ec16f29e
InsecureBeast <pe.dmitriev@gmail.com>
2023-02-28 11:26:40 +0300

    5 files modified, 1 added, 3 deleted

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from 'src/app/material.module';
+import { MaterialModule } from '../material.module';
 import { ControlsComponent } from './controls.component';
 
 @NgModule({
-  declarations: [
-    ControlsComponent
-  ],
-  exports: [
-    ControlsComponent
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ]
+  declarations: [ControlsComponent],
+  exports: [ControlsComponent],
+  imports: [CommonModule, MaterialModule],
 })
-export class ControlsModule { }
+export class ControlsModule {}
